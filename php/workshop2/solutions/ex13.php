3334edc905b65d4a4768b46a0e107b48ca5b3d98
cblop <commanderblop@gmail.com>
2017-11-20 12:52:53 +0000

    Added workshop 2 solutions

---
+++ php/workshop2/solutions/ex13.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Exercise 13</title>
+</head>
+<body>
+<?php
+
+//Define function.
+function hello(){
+  echo "Hello, World!";
+}
+
+//Call function.
+hello();
+
+?>
+</body>
+</html>
---
+++ php/workshop2/solutions/ex14.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Exercise 14</title>
+</head>
+<body>
+<?php
+
+//Define function.
+function recArea($l, $w){
+  $area = $l * $w;
+  echo "A rectangle of length $l and width $w has an area of $area.";
+}
+
+//Call function.
+recArea(2, 4);
+?>
+</body>
+</html>
